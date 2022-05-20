8eba3bab18a69df739a1afdaaa54405a43c60eca
SamirElbouzidi <96955744+SamirElbouzidi@users.noreply.github.com>
2022-05-20 21:43:34 +0200

    ajout de style

--- login.php
+++ login.php
@@ -2,25 +2,50 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php include 'includes/head.php'; ?>
+<link rel="stylesheet" href="login-style.css">
 <body>
-    
-    <br><br>
-    <form class="container" method="POST">
 
-        <?php if(isset($errorMsg)){ echo '<p>'.$errorMsg.'</p>'; } ?>
+<div id="blanc"> </div>
+<div id="blanc2"> </div>
+<div class="block_mere">
 
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Pseudo</label>
-            <input type="text" class="form-control" name="pseudo">
+    <!-- 1ere partie gauche logo+form ******************************************************-->
+    <div class="block_gauche">
+        <div> <img class="logo" src="Logo/Odyssée.png" > </div>
+        
+        <form class="log" method="POST">
+
+            <?php if(isset($errorMsg)){ echo '<p>'.$errorMsg.'</p>'; } ?>
+
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Pseudo</label>
+                <input type="text" class="form-control" name="pseudo" placeholder="pseudo">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" name="password" placeholder="password">
+            </div>
+            <button type="submit" class="btn btn-primary" name="validate">Se connecter</button> 
+            <div id="mdpHelp"> 
+                <a>Mot de passe ounlié</a>
+                <a>Help</a>
+            </div>
+        </form>
+        <p id="copy"> © Copyright 2022 Odyssée, Inc.<a href=""> Tous droit réservé</a></p>
+    </div> 
+    <!-- END 1ere partie gauche logo+form *************************************************-->
+
+    <!-- 2eme partie droite ******************************************************-->
+    <div class="block_droite">
+        <div>
+            <h1> Rejoignez-nous au sommet d' Odyssée !</h1>
+            <p>1er forum francophone pour les développeurs. rejoignez notre communauté riche et variée de plus de 5 000 utlisateurs.</p>
         </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" name="password">
-        </div>
-        <button type="submit" class="btn btn-primary" name="validate">Se connecter</button>
-        <br><br>
-        <a href="signup.php"><p>Je n'ai pas de compte, je m'inscris</p></a>
-    </form>
+        <div><a class="button" href="signup.php"><button ><p>S'inscrire maintenant</p></button></a></div>
+    </div>
+    <!-- END 2eme partie droite *************************************************-->
+
+</div> 
 
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -1,33 +1,55 @@
 <?php require('actions/users/signupAction.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="style.css">
 <?php include 'includes/head.php'; ?>
 <body>
-    <br><br>
-    <form class="container" method="POST">
+        
 
-        <?php if(isset($errorMsg)){ echo '<p>'.$errorMsg.'</p>'; } ?>
+            <div class="inscrire">
+                
+            <div class="float" > <img src="Logo/Ordi.png" class="img2"> </div>
+                    <form class="forum1" method="POST">    
+                    <div class="float1">  <img src="Logo/Odyssée.jpg" class="logo"></div>
+                    
+                    <h3>Inscrivez-vous à votre <br>forum de développement <br>Odyssé.</h3>
 
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Pseudo</label>
-            <input type="text" class="form-control" name="pseudo">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Nom</label>
-            <input type="text" class="form-control" name="lastname">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Prénom</label>
-            <input type="text" class="form-control" name="firstname">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control" name="password">
-        </div>
-        <button type="submit" class="btn btn-primary" name="validate">S'inscrire</button>
-        <br><br>
-        <a href="login.php"><p>J'ai déjà un compte, je me connecte</p></a>
-   </form>
+                        <?php if(isset($errorMsg)){ echo '<p>'.$errorMsg.'</p>'; } ?>
 
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Pseudo</label>
+                            <input type="text" class="form-control" name="pseudo" placeholder="Votre pseudo ">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Prénom</label>
+                            <input type="text" class="form-control" name="firstname" placeholder="Votre prénom ">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Nom</label>
+                            <input type="text" class="form-control" name="lastname" placeholder="Votre Nom ">
+                        </div>      
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Mot de passe</label>
+                            <input type="password" class="form-control" name="password" placeholder=" Votre mot de passe ">
+                        </div>
+                        <p>En vous inscrivant, vous confirmez avoir lu et accepté les conditions d'utilisation.</p>
+                        <button type="submit" class="btn btn-primary" name="validate">Enregistre-moi</button>
+                       
+                        <a href="login.php"><p>J'ai déjà un compte, je me connecte</p></a>
+                </form>    
+            </div>
+
+            <footer>
+                
+                    <p id= "Copy">Copyright 2022 Odyssée, Inc. <a href="">Tous droits réservés</a></p>
+
+                    <div id="foot">    
+                        <a  id="myA" class="cond" href="">Conditions d'utilisation</a>
+                        <a  id="myA" href="cond">Mention légal</a>
+                   </div>
+            
+              
+            </footer>  
+    
 </body>
 </html>
